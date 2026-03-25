d4ed84ba72ca642caf46eb2e6ca3b22168f18f7b
risiv404 <powalyaev2017@gmail.com>
2026-03-25 19:47:54 +0300

    Update teststest.spec.ts

--- qa-internship-tests/test/teststest.spec.ts
+++ qa-internship-tests/test/teststest.spec.ts
@@ -1,6 +1,17 @@
 import { test, expect } from '@playwright/test';
 
-test('Главная открывается', async ({ page }) => {
-  await page.goto('/');
-  await expect(page).toHaveURL(/.*list/);
+test('Фильтр по диапазону цен', async ({ page }) => {
+  await page.goto('https://cerulean-praline-8e5aa6.netlify.app/list');
+
+  await page.fill('input[placeholder="Мин цена"]', '100');
+  await page.fill('input[placeholder="Макс цена"]', '1000');
+  await page.click('button:has-text("Применить")');
+
+  const prices = await page.locator('.price').allTextContents();
+
+  for (const priceText of prices) {
+    const price = parseInt(priceText.replace(/\D/g, ''));
+    expect(price).toBeGreaterThanOrEqual(100);
+    expect(price).toBeLessThanOrEqual(1000);
+  }
 });
